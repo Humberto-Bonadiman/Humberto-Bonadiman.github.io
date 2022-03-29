3bca4c61cafbedcfbdf580fbdfa3839ead45c53d
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-03-28 20:33:49 -0400

    change again in react app

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './topbar.scss';
-// import PersonIcon from '@mui/icons-material/Person';
 import MailIcon from '@mui/icons-material/Mail';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
